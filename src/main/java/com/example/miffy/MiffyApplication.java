6dcf597c2bbd0383e8cf79686a5c4342dc96adef
lorelei47 <32691607+lorelei47@users.noreply.github.com>
2018-11-28 17:08:28 +0800

    springboot

---
+++ src/main/java/com/example/miffy/MiffyApplication.java
@@ -0,0 +1,12 @@
+package com.example.miffy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiffyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiffyApplication.class, args);
+	}
+}
